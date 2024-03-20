d795141785fdd2eea9f54527523d59bd9fcb2535
adrianbcaballero <abcaballero.py@gmail.com>
2024-03-19 20:21:42 -0700

    added ci/cd for frontend

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,13 +12,30 @@ resource "aws_s3_bucket" "wordtrendlearner_frontend" {
   bucket = "wordtrendlearner-frontend"
 }
 
-
-resource "aws_s3_bucket_policy" "origin" {
-  depends_on = [
-    aws_cloudfront_distribution.Site_Access
-  ]
+resource "aws_s3_bucket_ownership_controls" "wordtrendlearner_frontend" {
   bucket = aws_s3_bucket.wordtrendlearner_frontend.id
-  policy = data.aws_iam_policy_document.origin.json
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "wordtrendlearner_frontend" {
+  bucket = aws_s3_bucket.wordtrendlearner_frontend.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "wordtrendlearner_frontend" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.wordtrendlearner_frontend,
+    aws_s3_bucket_public_access_block.wordtrendlearner_frontend,
+  ]
+
+  bucket = aws_s3_bucket.wordtrendlearner_frontend.id
+  acl    = "public-read-write"
 }
 
 data "aws_iam_policy_document" "origin" {
@@ -26,6 +43,7 @@ data "aws_iam_policy_document" "origin" {
     aws_cloudfront_distribution.Site_Access,
     aws_s3_bucket.wordtrendlearner_frontend
   ]
+
   statement {
     sid    = "3"
     effect = "Allow"
